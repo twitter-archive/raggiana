7b2ffb3afdf27b1f4b2d22e03a94d96a3bf78184
Chris Aniszczyk <zx@twitter.com>
2013-08-27 17:19:05 -0500

    Fix Travis CI configuration
    Signed-off-by: Chris Aniszczyk <zx@twitter.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,8 @@ module.exports = function(grunt) {
     },
     karma: {
       unit: {
-        configFile: 'karma.conf.js'
+        configFile: 'karma.conf.js',
+        singleRun: true
       }
     },
     'gh-pages': {
