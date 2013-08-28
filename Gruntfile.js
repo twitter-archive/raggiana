87f1fe570f55a7bda48b48eb82027da99d2acf2a
Chris Aniszczyk <zx@twitter.com>
2013-08-28 16:52:09 -0500

    Configure Karma to run tests only in PhantomJS
    Signed-off-by: Chris Aniszczyk <zx@twitter.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,8 @@ module.exports = function(grunt) {
     karma: {
       unit: {
         configFile: 'karma.conf.js',
-        singleRun: true
+        singleRun: true,
+        browsers: ['PhantomJS']
       }
     },
     'gh-pages': {
